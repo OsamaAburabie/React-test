4cdea00a046e401c0a0646109cf81deb791fb23c
OsamaAburabie <aburabie.osama@gmail.com>
2020-11-18 03:40:55 +0200

    edited

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+// function Person(props) {
+// 	return (
+// 		<div>
+// 			<h2 className='Name' style={{}}>
+// 				Name: {props.guy.name}
+// 			</h2>
+// 			<h3 className='job' style={{ color: props.guy.job ? '' : 'red' }}>
+// 				Job: {props.guy.job}
+// 			</h3>
+// 		</div>
+// 	);
+// }
 
-function Person(props) {
-	return (
-		<div>
-			<h2 className='Name' style={{}}>
-				Name: {props.guy.name}
-			</h2>
-			<h3 className='job' style={{ color: props.guy.job ? '' : 'red' }}>
-				Job: {props.guy.job}
-			</h3>
-		</div>
-	);
+// export default Person;
+
+import React, { Component } from 'react';
+
+export default class Person extends Component {
+	render(props) {
+		return (
+			<div>
+				<h2 className='Name' style={{}}>
+					Name: {this.props.guy.name}
+				</h2>
+				<h3 className='job' style={{ color: this.props.guy.job ? '' : 'red' }}>
+					Job: {this.props.guy.job}
+				</h3>
+			</div>
+		);
+	}
 }
-
-export default Person;
